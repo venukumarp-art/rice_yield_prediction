53fe0eef6d9e345a55db03a1c0ec4274ff89e064
Venu Kumar <venukumarp@gmail.com>
2026-09-15 22:40:31 +0530

    Update train_model.py

--- src/train_model.py
+++ src/train_model.py
@@ -1,1 +1,12 @@
-
+import joblib
+from sklearn.linear_model import LinearRegression
+from config.settings import MODEL_PATH
+def train_model(df):
+  X=df[["land_size_acres"]].values
+  y=df["rice_packets"].values
+  model=LinearRegression()
+  model.fit(X,y)
+  joblib.dump(model,MODEL_PATH)# save the model to a pickle file
+  
+  
+  
