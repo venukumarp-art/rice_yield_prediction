0f901f161df50b77b2c33e2f7f06583ed6aeb367
Venu Kumar <venukumarp@gmail.com>
2026-09-15 22:06:24 +0530

    Update data_cleaning.py

--- src/data_cleaning.py
+++ src/data_cleaning.py
@@ -1,1 +1,10 @@
-
+# importing the libraries ,load the dataset,fill the data by using fillna method
+from config.settings import RAW_DATA_PATH
+from config.settings import PROCESSED_DATA_PATH
+import pandas as pd
+def clean_data():
+  df=pd.read_csv(RAW_DATA_PATH)
+  df["land_size_acres"]=df["land_size_acres"].fillna(df["land_size_acres")].mean())
+  df.to_csv(PROCESSED_DATA_PATH,index=False)
+  return df
+  
