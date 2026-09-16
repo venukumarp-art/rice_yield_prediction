2aa0da954ab268f15e18904f6d5a7b4344aea4cc
Venu Kumar <venukumarp@gmail.com>
2026-09-16 14:25:33 +0530

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,1 +1,13 @@
+from src.data_cleaning import clean_data
+from src.train_data import train_model
+from src.prediction import predict_rice_packets
+def main():
+  df=clean_data()
+  train_model(df)
+  predict_rice_packets()
+if __name__=="__main__":
+   main()
+  
+  
+
 
